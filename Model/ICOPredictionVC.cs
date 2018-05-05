758203e7b455c139c91d34c9b3a17dc19bc23a90
Kaleab <kaleabtessera@gmail.com>
2018-05-05 02:24:02 +0200

    Added nullable fields for PredictionVC

--- Model/ICOPredictionVC.cs
+++ Model/ICOPredictionVC.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
-
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Numerics;
 public class Category
 {
     public string naicsCode { get; set; }
@@ -13,7 +15,7 @@ public class Category
 
 public class Facebook
 {
-    public int likes { get; set; }
+    public int? likes { get; set; }
     public string handle { get; set; }
 }
 
@@ -27,7 +29,7 @@ public class Geo
     public string state { get; set; }
     public string postalCode { get; set; }
     public string city { get; set; }
-    public object subPremise { get; set; }
+    public string subPremise { get; set; }
     public string streetName { get; set; }
     public string streetNumber { get; set; }
 }
@@ -36,13 +38,13 @@ public class Metrics
 {
     public int? fiscalYearEnd { get; set; }
     public string estimatedAnnualRevenue { get; set; }
-    public object annualRevenue { get; set; }
+    public long? annualRevenue { get; set; }
     public int? raised { get; set; }
-    public object marketCap { get; set; }
+    public long? marketCap { get; set; }
     public string employeesRange { get; set; }
     public int? employees { get; set; }
-    public int alexaGlobalRank { get; set; }
-    public int alexaUsRank { get; set; }
+    public int? alexaGlobalRank { get; set; }
+    public int? alexaUsRank { get; set; }
 }
 
 public class Twitter
@@ -50,8 +52,8 @@ public class Twitter
     public string avatar { get; set; }
     public string site { get; set; }
     public string location { get; set; }
-    public int following { get; set; }
-    public int followers { get; set; }
+    public int? following { get; set; }
+    public int? followers { get; set; }
     public string bio { get; set; }
     public string id { get; set; }
     public string handle { get; set; }
@@ -76,26 +78,26 @@ public class Scores
 
 public class MonthMarket
 {
-    public List<double> price { get; set; }
-    public List<double> volume { get; set; }
+    public List<object> price { get; set; }
+    public List<object> volume { get; set; }
 }
 
 public class WeekMarket
 {
-    public List<double> price { get; set; }
-    public List<double> volume { get; set; }
+    public List<object> price { get; set; }
+    public List<object> volume { get; set; }
 }
 
 public class FourteenDay
 {
-    public double change { get; set; }
+    public double? change { get; set; }
     public object predictionStats { get; set; }
     public double prediction { get; set; }
 }
 
 public class SevenDay
 {
-    public double change { get; set; }
+    public double? change { get; set; }
     public object predictionStats { get; set; }
     public double prediction { get; set; }
 }
@@ -145,12 +147,15 @@ public class HighlightResult
 public class User
 {
     public string id { get; set; }
+    public string authkey { get; set; }
 }
 
 public class Hit
 {
+    [BsonId]
+    public ObjectId InternalId { get; set; }
     public object _reddit { get; set; }
-    public string _id { get; set; }
+    //public string _id { get; set; }
     public DateTime timeStamp { get; set; }
     public string imageUrl { get; set; }
     public int views { get; set; }
@@ -175,13 +180,13 @@ public class Hit
     public Geo _geo { get; set; }
     public Metrics _metrics { get; set; }
     public object _tags { get; set; }
-    public List<string> _tech { get; set; }
+    public List<object> _tech { get; set; }
     public Twitter _twitter { get; set; }
     public Crunchbase _crunchbase { get; set; }
     public Linkedin _linkedin { get; set; }
     public object market { get; set; }
     public double btcPrice { get; set; }
-    public double change { get; set; }
+    public double? change { get; set; }
     public object marketcap { get; set; }
     public double usdPrice { get; set; }
     public object volume { get; set; }
@@ -202,15 +207,15 @@ public class Hit
     public object news { get; set; }
     public List<object> team { get; set; }
     public List<object> verifiedTeam { get; set; }
-    public List<double> price7Day { get; set; }
-    public List<double> volume7Day { get; set; }
+    public List<object> price7Day { get; set; }
+    public List<object> volume7Day { get; set; }
     public string objectID { get; set; }
     public HighlightResult _highlightResult { get; set; }
     public string slackUrl { get; set; }
     public User user { get; set; }
-    public int? icoRaisedAmount { get; set; }
-    public int? tokensIco { get; set; }
-    public object tokensIssues { get; set; }
+    public double? icoRaisedAmount { get; set; }
+    public long? tokensIco { get; set; }
+    public BigInteger? tokensIssues { get; set; }
     public double? icoPriceUSD { get; set; }
     public string icoStartDate { get; set; }
     public string keywords { get; set; }
@@ -219,6 +224,14 @@ public class Hit
     public string teamMembers { get; set; }
     public string _foundedYear { get; set; }
     public string _legalName { get; set; }
+    public string events { get; set; }
+    public string facebookUrl { get; set; }
+    public string icoEndDate { get; set; }
+    public string slug { get; set; }
+    public string mediumUrl { get; set; }
+    public string releaseDate { get; set; }
+    public string projectFeatures { get; set; }
+    public string acceptedCurrencies { get; set; }
 }
 
 public class Result
